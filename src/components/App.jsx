d726ecd650011e870769b7ac51e2725b323f5754
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2024-01-16 21:55:42 +0100

    Update 1.4 add 1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import React from "react";
+import Profile from "./Profile/Profile";
+import user from './Data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+<Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+     stats={user.stats}
+     views={user.stats.views}
+     followers={user.stats.followers}
+     likes={user.stats.likes}
+
+/>
     </div>
-  );
+  )
 };
